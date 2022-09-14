a4ae12016a535630ddce6721843ae302ee47fb74
unknown <rafaelherdt1@gmail.com>
2022-09-14 16:19:57 -0300

    Stage 2/6 mobile version

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// componentes
+import { StatusBar } from 'react-native'
+import { Background } from './src/components/Background';
+import { Home } from './src/screens/Home';
+import { Loading } from './src/components/Loading';
+
+// fonts
+import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black } from '@expo-google-fonts/inter';
+
 
 export default function App() {
+
+  const [fontsLoad] = useFonts({
+    Inter_400Regular, 
+    Inter_600SemiBold, 
+    Inter_700Bold, 
+    Inter_900Black
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>HELLO WORLD!!!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <Background >
+
+      <StatusBar 
+          barStyle='light-content'
+          backgroundColor= 'transparent'
+          translucent
+      />
+
+
+        {fontsLoad ? <Home /> : <Loading />}
+    </Background>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
